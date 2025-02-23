e608d36f4dbc8e9169ec190882d4476ff0490de4
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-02-23 09:27:26 +0300

    working with sqlachemy on pythn

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
-from models import Dog
+from models import Dog, session
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    """Creates the table"""
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    """Saves a dog instance to the database"""
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    """Returns all dogs in the database"""
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    """Finds a dog by name"""
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    """Finds a dog by ID"""
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """Finds a dog by name and breed"""
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    """Updates a dog's breed"""
+    dog.breed = breed
+    session.commit()
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
+
+
+engine = create_engine("sqlite:///dogs.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
